52a0268b150f00776a6a612e7a1fa345efe40335
Erik Wright <github@oheriko.com>
2025-07-20 10:41:22 +0200

    add biome

--- flake.nix
+++ flake.nix
@@ -25,6 +25,7 @@
 
       languagePackages = {
         typescript = system: pkgs: [
+          pkgs.biome
           pkgs.bun
           pkgs.tailwindcss-language-server
           pkgs.typescript-language-server
